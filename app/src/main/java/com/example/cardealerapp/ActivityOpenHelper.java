915635de84987b4404c5d952a087a21913b23017
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-04 16:54:11 -0500

    Open Helper Class Added

--- app/src/main/java/com/example/cardealerapp/ActivityOpenHelper.java
+++ app/src/main/java/com/example/cardealerapp/ActivityOpenHelper.java
@@ -1,13 +1,35 @@
 package com.example.cardealerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
-public class ActivityOpenHelper extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityOpenHelper extends SQLiteOpenHelper {
+
+
+    public ActivityOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Clients(id text primary key, " +
+                "fullName text not null, email text not null, active text default 'On')");
+        db.execSQL("create table Vehicles(plate text primary key, " +
+                "model text not null, brand text not null, active text default 'On')");
+        db.execSQL("create table Sales(invoice text primary key, date text not null, " +
+                "id text not null, plate text not null, active text default 'On', constraint pk_sales " +
+                "foreign key(id) references Clients(id), foreign key(plate) references Vehicles(plate))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
