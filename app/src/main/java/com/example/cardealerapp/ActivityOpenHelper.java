d2f9aec5cf208234c249b1bbf59a180530135a90
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-04 16:35:43 -0500

    General Layout and Activities

---
+++ app/src/main/java/com/example/cardealerapp/ActivityOpenHelper.java
@@ -0,0 +1,13 @@
+package com.example.cardealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityOpenHelper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/cardealerapp/ActivitySales.java
@@ -0,0 +1,14 @@
+package com.example.cardealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales);
+    }
+}
