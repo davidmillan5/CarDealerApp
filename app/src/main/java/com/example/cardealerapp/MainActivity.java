7bc03c756285366da46d99ebfb59f75d3d1a43de
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-03-14 21:06:28 -0500

    Main Activity completed

--- app/src/main/java/com/example/cardealerapp/MainActivity.java
+++ app/src/main/java/com/example/cardealerapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cardealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void Clients(View view){
+        Intent intClients = new Intent(this,ActivityClients.class);
+        startActivity(intClients);
+    }
+
+    public void Vehicles(View view){
+        Intent intVehicles = new Intent(this, ActivityCars.class);
+        startActivity(intVehicles);
+    }
+
+    public void Sales(View view){
+        Intent intSales = new Intent(this, ActivitySales.class);
+        startActivity(intSales);
+    }
 
 }
