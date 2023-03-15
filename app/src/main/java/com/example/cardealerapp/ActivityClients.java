b1bb1a2605c9fe44cdf4438f87444bcaa75bc037
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-14 20:25:01 -0500

    Save Method Done

--- app/src/main/java/com/example/cardealerapp/ActivityClients.java
+++ app/src/main/java/com/example/cardealerapp/ActivityClients.java
@@ -2,9 +2,13 @@ package com.example.cardealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityClients extends AppCompatActivity {
 
@@ -16,7 +20,7 @@ public class ActivityClients extends AppCompatActivity {
 
     ActivityOpenHelper admin = new ActivityOpenHelper(this,"Cardealers.db",null,1);
 
-    long answers;
+    long response;
 
     byte sw;
 
@@ -24,5 +28,46 @@ public class ActivityClients extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clients);
+        getSupportActionBar().hide();
+
+        etidentification = findViewById(R.id.etidentification);
+        etfullname = findViewById(R.id.etfullname);
+        etemail = findViewById(R.id.etemail);
+        checkboxActive = findViewById(R.id.checkboxActive);
+        sw = 0;
+
     }
+
+
+    public void Save(View view){
+        identification = etidentification.getText().toString();
+        fullname = etfullname.getText().toString();
+        email = etemail.getText().toString();
+        if(identification.isEmpty() || fullname.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "Please enter all of the data require", Toast.LENGTH_SHORT).show();
+            etidentification.requestFocus();
+        }else{
+            SQLiteDatabase db = admin.getWritableDatabase();
+            ContentValues register = new ContentValues();
+            register.put("identification", identification);
+            register.put("fullname", fullname);
+            register.put("email",email);
+            if(sw== 0){
+                response = db.insert("Clients",null,register);
+            }else{
+                response = db.update("Clients",register,"identification = '"+identification+"'",null);
+                sw = 0;
+            }
+
+            if(response == 0){
+                Toast.makeText(this, "Error Saving The Information", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Register Save", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+
+
 }
