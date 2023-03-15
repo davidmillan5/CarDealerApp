082647474d600a054cf34cae1097c547dc18e4c8
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-14 20:00:55 -0500

    Database Created

--- app/src/main/java/com/example/cardealerapp/ActivityClients.java
+++ app/src/main/java/com/example/cardealerapp/ActivityClients.java
@@ -3,8 +3,22 @@ package com.example.cardealerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class ActivityClients extends AppCompatActivity {
+
+    EditText etidentification, etfullname, etemail;
+
+    CheckBox checkboxActive;
+
+    String identification, fullname, email;
+
+    ActivityOpenHelper admin = new ActivityOpenHelper(this,"Cardealers.db",null,1);
+
+    long answers;
+
+    byte sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
