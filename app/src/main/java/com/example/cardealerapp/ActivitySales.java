b857e63e4803dd51f1af188025912ce17ec1279a
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-15 19:13:23 -0500

    Working on Sales Activity

--- app/src/main/java/com/example/cardealerapp/ActivitySales.java
+++ app/src/main/java/com/example/cardealerapp/ActivitySales.java
@@ -3,8 +3,20 @@ package com.example.cardealerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ActivitySales extends AppCompatActivity {
+
+    EditText etinvoice, etdate, etIdClient, etCarPlate;
+
+    String invoice, date, id, plate;
+
+    ActivityOpenHelper admin = new ActivityOpenHelper(this,"Cardealers.db",null,1);
+
+    long response;
+
+    byte sw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
