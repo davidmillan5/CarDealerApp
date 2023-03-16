0a228390c272ea1f2a34d47e99f1e766e84b0092
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-15 19:23:08 -0500

    Started to work on the Methods in the Sales Activity

--- app/src/main/java/com/example/cardealerapp/ActivitySales.java
+++ app/src/main/java/com/example/cardealerapp/ActivitySales.java
@@ -3,7 +3,9 @@ package com.example.cardealerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivitySales extends AppCompatActivity {
 
@@ -22,5 +24,35 @@ public class ActivitySales extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
+
+        etinvoice = findViewById(R.id.etinvoice);
+        etdate = findViewById(R.id.etdate);
+        etIdClient = findViewById(R.id.etIdClient);
+        etCarPlate = findViewById(R.id.etCarPlate);
+        sw = 0;
+
     }
+
+    public void Save(View view){
+        invoice = etinvoice.getText().toString();
+        date = etdate.getText().toString();
+        id = etIdClient.getText().toString();
+        plate = etCarPlate.getText().toString();
+
+        if(invoice.isEmpty() || date.isEmpty() || id.isEmpty() || plate.isEmpty()){
+            Toast.makeText(this, "You need to enter all of the information!", Toast.LENGTH_SHORT).show();
+            etinvoice.requestFocus();
+        }else{
+
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
